6cfd576c68aa4a5d6d5b576ea1a7903c1d969428
Miguel084 <mi084@icloud.com>
2023-11-26 14:16:32 -0300

    Refactor form submission functionality

--- app.js
+++ app.js
@@ -1,34 +1,65 @@
-class FormSubmit{
-    constructor(settigns){
-        this.settigns = settigns;
-        this.form = document.querySelector(settigns.form);
-        this.formButton = document.querySelector(settigns.button);
-
-        if(this.form){
-            this.url = this.form.getAttribute("action")
-        }
-
+class FormSubmit {
+    constructor(settings) {
+      this.settings = settings;
+      this.form = document.querySelector(settings.form);
+      this.formButton = document.querySelector(settings.button);
+      if (this.form) {
+        this.url = this.form.getAttribute("action");
+      }
+      this.sendForm = this.sendForm.bind(this);
     }
-
-    displaySuccess(){
-        this.form.innerHTML = this.settigns.success;
-
+  
+    displaySuccess() {
+      this.form.innerHTML = this.settings.success;
     }
-
-    displayError(){
-        this.form.innerHTML = this.settigns.error;
+  
+    displayError() {
+      this.form.innerHTML = this.settings.error;
     }
-    
-    int(){
-        if (this.form) this.formButton.addEventListener("click",()=> this.displaySuccess());
-        return this;
+  
+    getFormObject() {
+      const formObject = {};
+      const fields = this.form.querySelectorAll("[name]");
+      fields.forEach((field) => {
+        formObject[field.getAttribute("name")] = field.value;
+      });
+      return formObject;
     }
-
-}
-
-const formSubmit = new FormSubmit({
+  
+    onSubmission(event) {
+      event.preventDefault();
+      event.target.disabled = true;
+      event.target.innerText = "Enviando...";
+    }
+  
+    async sendForm(event) {
+      try {
+        this.onSubmission(event);
+        await fetch(this.url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+          body: JSON.stringify(this.getFormObject()),
+        });
+        this.displaySuccess();
+      } catch (error) {
+        this.displayError();
+        throw new Error(error);
+      }
+    }
+  
+    init() {
+      if (this.form) this.formButton.addEventListener("click", this.sendForm);
+      return this;
+    }
+  }
+  
+  const formSubmit = new FormSubmit({
     form: "[data-form]",
     button: "[data-button]",
-    success: "<h1 class = 'sucesso'>Mensagem enviada!</h1>",
-    error: "<h1 class = 'erro'>Erro ao enviar mensagem!</h1>",
-});
+    success: "<h1 class='success'>Mensagem enviada!</h1>",
+    error: "<h1 class='error'>Não foi possível enviar sua mensagem.</h1>",
+  });
+  formSubmit.init();
